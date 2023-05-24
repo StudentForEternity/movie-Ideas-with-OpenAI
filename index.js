142f45aba83556a25342670d912d4396e8992552
StudentForEternity <109116999+StudentForEternity@users.noreply.github.com>
2023-05-23 23:44:22 -0400

    training my AI model
    using examples of better outputs to train my AI model to give better responses.

--- index.js
+++ index.js
@@ -17,13 +17,51 @@ document.getElementById("send-btn").addEventListener("click", () => {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
     fetchBotReply(userPrompt)
+    fetchSynopsis(userPrompt)
   }
 })
 
 async function fetchBotReply(userPrompt) {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${userPrompt}`,
+    prompt: `Generate a short message to enthusiastically say that user's prompt 
+            sounds interesting and that you need some minutes to think about it. 
+            Mention one aspect of the sentence.
+            ###
+            user's prompt: A group of corrupt lawyers try to send an innocent woman to jail.
+            message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+            ### 
+            user's prompt: ${userPrompt}
+            message: 
+            `,
+    max_tokens: 60,
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
+}
+
+async function fetchSynopsis(userPrompt) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `
+    ###
+    Movie idea: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    Movie Synopsis: The Top Gun Naval Fighter Weapons School is where the 
+      best of the best train to refine their elite flying skills. When hotshot 
+      fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude
+      and cocky demeanor put him at odds with the other pilots, especially the cool and 
+      collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter 
+      pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte 
+      Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - 
+      and also the love of Charlotte, but struggles to balance his personal and professional life. 
+      As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons 
+      and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from 
+      the mission triumphant.
+    ###
+    Movie idea:${userPrompt}
+    Movie synopsis: `,
+    max_tokens: 700,
+  })
+  console.log(response)
+  document.getElementById("output-text").innerText =
+    response.data.choices[0].text.trim()
 }
